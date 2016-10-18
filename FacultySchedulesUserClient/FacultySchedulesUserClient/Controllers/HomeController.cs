35b8c2321d203fafb6194c5bfc7b8f94c36a6d54
David Crawford <crawford@mail.gvsu.edu>
2016-10-17 21:23:33 -0400

    Scraper functions properly now.

--- FacultySchedulesUserClient/FacultySchedulesUserClient/Controllers/HomeController.cs
+++ FacultySchedulesUserClient/FacultySchedulesUserClient/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Mvc.Ajax;
+using HtmlAgilityPack;
 
 namespace FacultySchedulesUserClient.Controllers
 {
 	public class HomeController : Controller
 	{
+		//List<List<string>> somelist = new List<List<string>>();
 		public ActionResult Index()
 		{
+			Scrape scraper = new Scrape();
+			List<HtmlNode> x = scraper.BeginScrape();
+
+			foreach (HtmlNode node in x)
+			{
+				List<HtmlNode> s = node.Elements("td").ToList();
+				foreach (HtmlNode item in s)
+				{
+					Console.WriteLine("TD Value: " + item.InnerText);
+				}
+			}
+			Console.ReadLine();
+
+
+
 			var mvcName = typeof(Controller).Assembly.GetName();
 			var isMono = Type.GetType("Mono.Runtime") != null;
 
