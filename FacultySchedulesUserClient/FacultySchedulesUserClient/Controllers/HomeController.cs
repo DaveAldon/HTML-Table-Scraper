ab33e840c732716e2be1d538c696107d26caded2
David Crawford <crawford@mail.gvsu.edu>
2016-10-22 17:16:23 -0400

    Working on getting connected to a database.

--- FacultySchedulesUserClient/FacultySchedulesUserClient/Controllers/HomeController.cs
+++ FacultySchedulesUserClient/FacultySchedulesUserClient/Controllers/HomeController.cs
@@ -14,81 +14,124 @@ namespace FacultySchedulesUserClient.Controllers
 		List<string> wedList = new List<string>();
 		List<string> thuList = new List<string>();
 		List<string> friList = new List<string>();
+
+		List<string> dayList = new List<string> { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+		List<string> days = new List<string>();
+		int index = 0;
+		string time = "";
+		string value = "";
+		string poo = "";
+
 		//List<List<string>> somelist = new List<List<string>>();
 		public ActionResult Index()
 		{
 			Scrape scraper = new Scrape();
 			List<HtmlNode> x = scraper.BeginScrape();
 
-			foreach (HtmlNode node in x)
+			bool first = true;
+			foreach (HtmlNode item in x)
 			{
-				
-				int dayCount = 0;
-				List<HtmlNode> s = node.Elements("td").ToList();
-				foreach (HtmlNode item in s)
+				if (first)
 				{
-					if (dayCount == 0)
-					{
-						timeList.Add(item.InnerText);
-					}
-					else if (dayCount == 1)
-					{
-						if (item.InnerText == "&nbsp;")
-						{
-							monList.Add("Empty");
-						}
-						else
-						monList.Add(item.InnerText);
-					}
-					else if (dayCount == 2)
-					{
-						if (item.InnerText == "&nbsp;")
-						{
-							tueList.Add("Empty");
-						}
-						else
-						tueList.Add(item.InnerText);
-					}
-					else if (dayCount == 3)
-					{
-						if (item.InnerText == "&nbsp;")
-						{
-							wedList.Add("Empty");
-						}
-						else
-						wedList.Add(item.InnerText);
-					}
-					else if (dayCount == 4)
-					{
-						if (item.InnerText == "&nbsp;")
-						{
-							thuList.Add("Empty");
-						}
-						else
-						thuList.Add(item.InnerText);
-					}
-					else if (dayCount == 5)
-					{
-						if (item.InnerText == "&nbsp;")
-						{
-							friList.Add("Empty");
-						}
-						else
-						friList.Add(item.InnerText);
-					}
-					dayCount++;
+					first = false;
 				}
-				dayCount = 0;
+
+				List<HtmlNode> cell = item.Elements("td").ToList();
+				foreach (HtmlNode events in cell)
+				{
+					value = events.InnerText;
+					if (index == 0)
+					{
+						index = index + 1;
+						time = value;
+						continue;
+					}
+
+					days.Add(dayList[index - 1] + " " + time + " " + value);
+					index = index + 1;
+				}
+				index = 0;
 			}
 
+			/*
+			foreach (HtmlNode node in x)
+			{
+				List<HtmlNode> s = node.Elements("tr").ToList();
+				foreach (HtmlNode item in s)
+				{
+					int dayCount = 0;
 
-			var mvcName = typeof(Controller).Assembly.GetName();
-			var isMono = Type.GetType("Mono.Runtime") != null;
+					List<HtmlNode> r = node.Elements("td").ToList();
+					foreach (HtmlNode cell in r)
+					{
+						if (dayCount == 0)
+						{
+							timeList.Add(item.InnerText);
+						}
+						else if (dayCount == 1)
+						{
+							if (item.InnerText == "&nbsp;")
+							{
+								monList.Add("Empty");
+							}
+							else
+								monList.Add(item.InnerText);
+						}
+						else if (dayCount == 2)
+						{
+							if (item.InnerText == "&nbsp;")
+							{
+								tueList.Add("Empty");
+							}
+							else
+								tueList.Add(item.InnerText);
+						}
+						else if (dayCount == 3)
+						{
+							if (item.InnerText == "&nbsp;")
+							{
+								wedList.Add("Empty");
+							}
+							else
+								wedList.Add(item.InnerText);
+						}
+						else if (dayCount == 4)
+						{
+							if (item.InnerText == "&nbsp;")
+							{
+								thuList.Add("Empty");
+							}
+							else
+								thuList.Add(item.InnerText);
+						}
+						else if (dayCount == 5)
+						{
+							if (item.InnerText == "&nbsp;")
+							{
+								friList.Add("Empty");
+							}
+							else
+								friList.Add(item.InnerText);
+						}
+						dayCount++;
+					}
+				}
+				}
+				*/
 
-			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor + thuList[0];
-			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+			for (int i = 0; i < days.Count; i++)
+			{
+				poo = poo + days[i] + "\r\n";
+			}
 
-			return View();
+		var mvcName = typeof(Controller).Assembly.GetName();
+		var isMono = Type.GetType("Mono.Runtime") != null;
+
+		ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor + poo;
+		ViewData["Runtime"] = isMono? "Mono" : ".NET";
+
+		return View();
 		}
 	}
 }
---
+++ FacultySchedulesUserClient/FacultySchedulesUserClient/GiveData.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace FacultySchedulesUserClient
+{
+	public class GiveData
+	{
+
+		public void DBpush()
+		{
+			string connectionParam = "server=127.0.0.1;uid=root;port=8889;pwd=root;database=Faculty;";
+			MySqlConnection connection = null;
+			MySqlDataReader dataReader = null;
+
+			try
+			{
+				connection = new MySqlConnection(connectionParam);
+				connection.Open();
+				//string stm = "INSERT INTO trax (Entry, Name, PO, IDC) VALUES(@Entry, @Name, @PO, @IDC)";
+				string stm = "INSERT INTO IraWoodring (day, hour, event) VALUES(@day, @hour, @event)";
+
+				MySqlCommand cmd = new MySqlCommand(stm, connection);
+				cmd.Parameters.AddWithValue("@day", DSSTstring);
+				cmd.Parameters.AddWithValue("@hour", POstring);
+				cmd.Parameters.AddWithValue("@event", IDCstring);
+				dataReader = cmd.ExecuteReader();
+			}
+			catch (MySqlException error)
+			{
+
+			}
+			finally //We need to close all of our connections once everything is retrieved
+			{
+				if (dataReader != null)
+				{
+					dataReader.Close();
+				}
+
+				if (connection != null)
+				{
+					connection.Close();
+				}
+			}
+		}
+	}
+}
