51268ebdeec275e7f51d2e1531da375b0ed7b885
David Crawford <crawford@mail.gvsu.edu>
2016-11-21 00:29:02 -0500

    Reorganization.

---
+++ Faculty Schedules/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿//
+//This class handles the beginning and end cycles of the application.
+//
+
+using AppKit;
+using Foundation;
+
+namespace FacultySchedules
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : NSApplicationDelegate
+	{
+		MainWindowController mainWindowController;
+
+		public override void DidFinishLaunching(NSNotification notification)
+		{
+			mainWindowController = new MainWindowController();
+			mainWindowController.Window.MakeKeyAndOrderFront(this);
+		}
+
+		public override void WillTerminate(NSNotification notification) {}
+	}
+}
---
+++ Faculty Schedules/Main.cs
@@ -0,0 +1,17 @@
+﻿//
+//This class runs the instantiation of the entire project.
+//
+
+using AppKit;
+
+namespace FacultySchedules
+{
+	static class MainClass
+	{
+		static void Main(string[] args) //Main window is fed into this class
+		{
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
